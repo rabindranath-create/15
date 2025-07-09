037d965d65786c87ef12884843f41b64f6d41e44
rabindranath-create <87770453+rabindranath-create@users.noreply.github.com>
2025-07-09 17:20:34 -0500

    Update main_script752.R

--- main_script752.R
+++ main_script752.R
@@ -45,7 +45,7 @@ for (i in 1:100) {
   result <- MACS_Alg_M(obs_gen_para, kappa = 7, lambda, cost)
 
   results_3[i, ] <- list(
-    Run = i
+    Run = i,
     N_t = 75,
     Length = result$Length_total,
     Cost = result$Cost_total,
@@ -74,7 +74,7 @@ for (i in 1:100) {
   result <- MACS_Alg_M(obs_gen_para, kappa = 7, lambda, cost)
 
   results_4[i, ] <- list(
-    Run = i
+    Run = i,
     N_t = 100,
     Length = result$Length_total,
     Cost = result$Cost_total,
